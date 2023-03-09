f1c0b0f04d153490ea7c0a71b421a945fce2730d
AnthLima <tryout3699@gmail.com>
2023-03-09 02:26:57 -0300

    [feat][main]: Add a simple video background

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,19 @@ import {
   PopoverDomRef,
   TextArea,
 } from "@ui5/webcomponents-react";
-import styles from "./App.modules.scss";
+import styles from "./App.module.scss";
 import SearchTextArea from "./components/SearchTextArea/SearchTextArea";
+import videoBg from "./assets/videoBg.mp4";
 export default function App() {
   return (
     <ThemeProvider>
-      <SearchTextArea />
+      <div className={styles.main}>
+        <div className={styles.overlay}></div>
+        <video src={videoBg} autoPlay loop muted />
+        <div className={styles.content}>
+          <SearchTextArea />
+        </div>
+      </div>
     </ThemeProvider>
   );
 }
